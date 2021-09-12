ec685981f51b52b558ea808b88cab4336eef0ade
N59482 <iduginmail@ya.ru>
2021-09-12 16:28:37 +0300

    add LSWRC.cpp

--- LSWRC.cpp
+++ LSWRC.cpp
@@ -1,48 +1,27 @@
 class Solution {
-public:
-    void showStr(string str)
-    {
-        for(char cStr : str)
-        {
-            cout<<cStr<<"-";
-        };
-        cout<<"end"<<endl;
-    };
-
+public:    
     int lengthOfLongestSubstring(string s) 
     {
-        int max = 0, i = 0;
+        int max = 0;
         string tempStr;
         if (!s.empty())
             {
-                cout<<"s :"<<endl;
-                showStr(s);
-
-                cout<<"first->"<<s.at(0)<<"<-first\n";
-
                 tempStr = s.at(0); // присваиваем первый элемент строки 
-                i = 1; // счетчик делаем 1
                 s.erase(0,1);// удаляем первый элемент строки
-
-                cout<<"tempStr :"<<endl;
-                showStr(tempStr);
-
-                cout<<"s :"<<endl;
-                showStr(s);
-
-
-                for(char cs : s) // cs - char string (global)
+                for(char cs : s) // cs - char of string s 
                     {
-                        cout<<"->"<<cs<<"<-\n";
-                        if (int(tempStr.find(cs)) < 0) // если символ не найден то запоминаем его и увеличиваем счетчик 
+                        int pos = tempStr.find(cs); // если символ не уникален запоминаем его позицию в строке если нет то find вернет отрицательное число
+                        tempStr.push_back(cs); // сразу запоминаем новй символ. 
+                        if (pos >= 0) // если символ не уникален
                             {
-                                cout<<"no one - "<<cs<<endl;
-                                tempStr.push_back(cs);
-                                i++;
-                                showStr(tempStr);
-                            } else {cout<<"found - "<<cs<<" on pos "<<tempStr.find(cs)<<endl;};
+                                cout<<"MAX = "<<max<<" lenght = "<<tempStr.size()<<endl;
+                                if (max < (tempStr.size() - 1)) max = (tempStr.size() - 1); // запоминаем самую длинную подстроку (-1 потому что уже был выполнен push_back).
+                                cout<<"MAX = "<<max<<" lenght = "<<tempStr.size()<<endl; 
+                                tempStr.erase(0,pos+1); // оБрезаем строку до повторяющего символа (в памяти остаются только уникальные символы)
+                            };
                     };
-            }        
+            };
+        if (max < tempStr.size()) max = tempStr.size();  // запоминаем самую длинную подстроку (на случай если последний символ был уникальный)
         return max;
     };
 };
